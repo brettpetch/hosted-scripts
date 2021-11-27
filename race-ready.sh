6ecd9985ce34d013ad3eb7621ec036fe08fc7e74
Brett Petch <brettpetch@icloud.com>
2021-11-27 14:38:42 -0500

    fix: seds to actually work lol

--- race-ready.sh
+++ race-ready.sh
@@ -5,7 +5,7 @@ mkdir -p ~/.logs/
 touch ~/.logs/lounge.log
 log="$HOME/.logs/race-ready.log"
 port=$(grep 'WebUI\\Port' /home/${user}/.config/qBittorrent/qBittorrent.conf | cut -d= -f2)
-subnet=$(cat /install/subnet.lock)
+subnet=$(cat /home/${user}/.install/subnet.lock)
 
 function _nvm() {
     if [[ ! -d /home/$user/.nvm ]]; then
@@ -30,11 +30,11 @@ function qbit_race() {
     RACE_DIR="/home/${user}/scripts/qbit-race"
     git clone https://github.com/ckcr4lyf/qbit-race.git "${RACE_DIR}" >> "$log" 2>&1
     cp "${RACE_DIR}/sample.env" "${RACE_DIR}/.env"
-    sed -i 's|QBIT_HOST=127.0.0.1|QBIT_HOST=${subnet}|g' "${RACE_DIR}/.env"
-    sed -i 's|QBIT_PORT=8080|QBIT_PORT=${port}|g' "${RACE_DIR}/.env"
+    sed -i "s|QBIT_HOST=127.0.0.1|QBIT_HOST=${subnet}|g" "${RACE_DIR}/.env"
+    sed -i "s|QBIT_PORT=8080|QBIT_PORT=${port}|g" "${RACE_DIR}/.env"
     cp "${RACE_DIR}/sample.settings.js" "${RACE_DIR}/settings.js"
-    sed -i 's|CONCURRENT_RACES: 1,|CONCURRENT_RACES: 3,|g' "${RACE_DIR}/settings.js"
     sed -i 's|REANNOUNCE_INTERVAL: 5000,|REANNOUNCE_INTERVAL: 7000,|g' "${RACE_DIR}/settings.js"
+    sed -i 's|CONCURRENT_RACES: 1,|CONCURRENT_RACES: 3,|g' "${RACE_DIR}/sample.settings.js"
     npm --prefix "${RACE_DIR}" install "${RACE_DIR}" >> "$log" 2>&1
     echo "Your script path is $(which node)"
     echo "Your script argument is '${RACE_DIR}/bin/autodl_feed.js \"\$(InfoHash)\" \"\$(InfoName)\" \"\$(Tracker)\" \"\$(TorrentPathName)\"'"
@@ -44,9 +44,11 @@ function qbit_race() {
 }
 
 function nightwalker() {
+    echo "Cloning Nightwalker Repo"
     git clone "https://github.com/brettpetch/nightwalker.git" /home/${user}/scripts/nightwalker >> "$log" 2>&1
+    echo "Nightwalker cloned."
     sudo box stop qbittorrent
-    sed -i 's|WebUI\\RootFolder=.*|WebUI\\RootFolder=/home/${user}/scripts/nightwalker/|g' /home/${user}/.config/qBittorrent/qBittorrent.conf
+    sed -i "s|^.*RootFolder.*|WebUI\\\RootFolder=/home/${user}/scripts/nightwalker/|g" /home/${user}/.config/qBittorrent/qBittorrent.conf
     sudo box start qbittorrent
     touch ~/.install/.nightwalker.lock
 }
