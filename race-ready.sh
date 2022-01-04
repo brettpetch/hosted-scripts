9ec63593c9c361b469138b79cac56fab3a938ef9
Brett Petch <brettpetch@icloud.com>
2022-01-04 13:13:23 -0500

    refactor node install

--- race-ready.sh
+++ race-ready.sh
@@ -7,24 +7,22 @@ log="/home/${user}/.logs/race-ready.log"
 port=$(grep 'WebUI\\Port' /home/${user}/.config/qBittorrent/qBittorrent.conf | cut -d= -f2)
 subnet=$(cat /home/${user}/.install/subnet.lock)
 
-function _nvm() {
+function _deps() {
+    ## Function for installing nvm.
     if [[ ! -d /home/$user/.nvm ]]; then
-        echo "Installing NVM and Node..."
+        echo "Installing node"
         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash >> "$log" 2>&1
-        export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-        nvm install --lts >> "$log" 2>&1 || {
-            echo "Node failed to install"
-            exit 1
-        }
-        echo "Node installed."
-        npm install -g npm@latest "$log" 2>&1
+        echo "nvm installed."
     else
-        echo "Node is already installed."
-        echo "Updating npm..."
-        npm install -g npm@latest "$log" 2>&1
-        echo "npm updated..."
+        echo "nvm is already installed."
     fi
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    nvm install --lts >> "$log" 2>&1 || {
+        echo "node failed to install"
+        exit 1
+    }
+    echo "Node LTS installed."
 }
 
 function qbit_race() {
