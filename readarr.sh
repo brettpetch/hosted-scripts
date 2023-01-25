1c2446ab5bfc3f8d9b70e79ec03d8c9f40155076
Brett Petch <brettpetch@icloud.com>
2023-01-25 10:31:16 -0500

    fix: add disclaimer

--- readarr.sh
+++ readarr.sh
@@ -110,6 +110,18 @@ function _remove() {
     echo "Readarr has been removed."
 }
 
+echo 'This is unsupported software. You will not get help with this, please answer `yes` if you understand and wish to proceed'
+if [[ -z ${eula} ]]; then
+    read -r eula
+fi
+
+if ! [[ $eula =~ yes ]]; then
+  echo "You did not accept the above. Exiting..."
+  exit 1
+else
+  echo "Proceeding with installation"
+fi
+
 echo "Welcome to Readarr installer..."
 echo ""
 echo "What do you like to do?"
