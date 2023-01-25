ea6eb04df4fc86f2bb701caad2fd4fdedaf862ca
Brett Petch <brettpetch@icloud.com>
2023-01-25 10:28:51 -0500

    fix: add disclaimer

--- mylar3.sh
+++ mylar3.sh
@@ -178,6 +178,17 @@ function _upgrade(){
     echo "Upgrade Complete!"
 }
 
+echo 'This is unsupported software. You will not get help with this, please answer `yes` if you understand and wish to proceed'
+if [[ -z ${eula} ]]; then
+    read -r eula
+fi
+
+if ! [[ $eula =~ yes ]]; then
+  echo "You did not accept the above. Exiting..."
+  exit 1
+else
+  echo "Proceeding with installation"
+fi
 
 echo "Welcome to the Mylar3 installer..."
 echo ""
