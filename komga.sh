55049de1356ade2c9dac9a4a3fc243fe3078a83c
Brett Petch <brettpetch@icloud.com>
2023-01-25 10:27:56 -0500

    fix: add disclaimer

--- komga.sh
+++ komga.sh
@@ -92,6 +92,18 @@ function _remove() {
     rm -f "$HOME/.install/.komga.lock"
 }
 
+echo 'This is unsupported software. You will not get help with this, please answer `yes` if you understand and wish to proceed'
+if [[ -z ${eula} ]]; then
+    read -r eula
+fi
+
+if ! [[ $eula =~ yes ]]; then
+  echo "You did not accept the above. Exiting..."
+  exit 1
+else
+  echo "Proceeding with installation"
+fi
+
 echo "Welcome to the Komga installer..."
 echo ""
 echo "What do you like to do?"
