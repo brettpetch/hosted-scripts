f3899d158068408254ae973e6b35aadc28ec1b86
Brett Petch <brettpetch@icloud.com>
2023-01-25 10:29:07 -0500

    fix: add disclaimer

--- ngpost.sh
+++ ngpost.sh
@@ -81,6 +81,18 @@ function _remove() {
     echo "ngPost has been removed successfully!"
 }
 
+echo 'This is unsupported software. You will not get help with this, please answer `yes` if you understand and wish to proceed'
+if [[ -z ${eula} ]]; then
+    read -r eula
+fi
+
+if ! [[ $eula =~ yes ]]; then
+  echo "You did not accept the above. Exiting..."
+  exit 1
+else
+  echo "Proceeding with installation"
+fi
+
 echo "Welcome to The ngpost installer..."
 echo ""
 echo "What do you like to do?"
