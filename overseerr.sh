3c3d77046a13687d3704c71c49d08335a16e8f37
Brett Petch <brettpetch@icloud.com>
2023-01-25 10:29:43 -0500

    fix: add disclaimer

--- overseerr.sh
+++ overseerr.sh
@@ -111,6 +111,18 @@ function _remove() {
     rm -rf ~/.install/.overseerr.lock
 }
 
+echo 'This is unsupported software. You will not get help with this, please answer `yes` if you understand and wish to proceed'
+if [[ -z ${eula} ]]; then
+    read -r eula
+fi
+
+if ! [[ $eula =~ yes ]]; then
+  echo "You did not accept the above. Exiting..."
+  exit 1
+else
+  echo "Proceeding with installation"
+fi
+
 echo "Welcome to the Overseerr installer..."
 echo ""
 echo "What do you like to do?"
