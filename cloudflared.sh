13577783eb53fe2a723753147517a33bd4b11d67
Brett Petch <brettpetch@icloud.com>
2025-03-16 20:30:42 -0400

    cleanup(cloudflared): Provide Requirements before install

--- cloudflared.sh
+++ cloudflared.sh
@@ -14,24 +14,16 @@ function github_latest_version() {
 }
 
 function _install() {
-    echo "Welcome to the cloudflared installer for HBD/Swizzin.net Appbox Environments"
-    echo "This application requires that you have a Cloudflare account with a (sub)domain that can be utilized for services running on your slot."
-    echo "If you don't have a domain, you can get one from https://porkbun.com (cheap) or https://njal.la (privacy oriented)"
-    echo "You will then need to sign up for a free Cloudflare account at https://dash.cloudflare.com/"
-    echo "Once you have your domain setup on Cloudflare, you will need to create your zero trust organization."
-    echo "After you have created your organization, you will need to create a tunnel."
-    echo "You may obtain your Zero Trust Token from https://one.dash.cloudflare.com/"
-    echo "Select the account this is applicable to, then select 'Networks' then 'Tunnels' and create a new tunnel"
-
-    mkdir -p "$HOME/.local/bin"
     echo "Downloading cloudflared"
+    mkdir -p "$HOME/.local/bin"
     latest_version=$(github_latest_version cloudflare/cloudflared)
     curl -s -L "https://github.com/cloudflare/cloudflared/releases/download/${latest_version}/cloudflared-linux-amd64" -o "$HOME/.local/bin/cloudflared"
     chmod +x "$HOME/.local/bin/cloudflared"
 
-    read -rep "Please enter your Cloudflare Zero Trust Tunnel Token:" CF_API_TOKEN 
+    read -rep "Please enter your Cloudflare Zero Trust Tunnel Token: " CF_API_TOKEN 
     mkdir -p "$HOME/.config/cloudflared/"
     touch "$HOME/.config/cloudflared/config.yml"
+    echo "Installing Systemd Service"
     mkdir -p "$HOME/.config/systemd/user/"
 
     cat >> "$HOME/.config/systemd/user/cloudflared.service" <<EOF
@@ -48,9 +40,11 @@ RestartSec=10
 WantedBy=default.target
 EOF
 
+    echo "Enabling cloudflared systemd service"
     systemctl --user enable --now cloudflared
     echo "Successfully installed cloudflared."
     echo "You may continue with your configuration at https://one.dash.cloudflare.com/ - you will likely want to set your public hostnames to applications that you want to expose outside of your slot."
+    echo "Limited documentation available at https://github.com/brettpetch/hosted-scripts/wiki/Cloudflared"
     echo "Your subnet ip is $(cat $HOME/.install/subnet.lock)"
 }
 
@@ -75,8 +69,14 @@ else
   echo "Proceeding with installation"
 fi
 
-echo "Welcome to the Cloudflared installer..."
-echo ""
+echo "Welcome to the cloudflared installer for HBD/Swizzin.net Appbox Environments"
+echo "This application requires that you have a Cloudflare account with a (sub)domain that can be utilized for services running on your slot."
+echo "If you don't have a domain, you can get one from https://porkbun.com (cheap) or https://njal.la (privacy oriented)"
+echo "You will then need to sign up for a free Cloudflare account at https://dash.cloudflare.com/"
+echo "Once you have your domain setup on Cloudflare, you will need to create your zero trust organization."
+echo "After you have created your organization, you will need to create a tunnel."
+echo "You may obtain your Zero Trust Token from https://one.dash.cloudflare.com/"
+echo "Select the account this is applicable to, then select 'Networks' then 'Tunnels' and create a new tunnel"
 echo "What do you like to do?"
 echo "Logs are stored at ${log}"
 echo "install = Install Cloudflared"
